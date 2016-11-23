70f733094098a85d851454d25133b0f37046deea
David <d.giger.m@gmail.com>
2016-11-23 10:42:07 +0100

    UI adjustments

--- main/app/footer.php
+++ main/app/footer.php
@@ -1,3 +1,9 @@
-<footer>
-
-</footer>
+<nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        <ul class="nav navbar-nav">
+            <li>
+                <a href="">info</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- main/app/header.php
+++ main/app/header.php
@@ -1,26 +1,29 @@
 <nav class="navbar navbar-default">
     <div class="container">
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li>
-                    <?php echo '<a class="btn" href="?controller=Home&action=show">Home</a>' ?>
-                </li>
-                <li>
-                    <?php echo '<a class="btn" href="?controller=University&action=show">Universities</a>' ?>
-                </li>
-                <li>
-                    <?php echo '<a class="btn" href="?controller=Course&action=show">Courses</a>' ?>
-                </li>
-            </ul>
-            <form class="navbar-form navbar-right">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="email">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="password">
-                </div>
-                <button type="submit" class="btn btn-default">Login</button>
-            </form>
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+                <img alt="Brand" src="../../img/logo.png" height="20px">
+            </a>
         </div>
+        <ul class="nav navbar-nav">
+            <li>
+                <?php echo '<a class="btn" href="?controller=Home&action=show">Home</a>' ?>
+            </li>
+            <li>
+                <?php echo '<a class="btn" href="?controller=University&action=show">Universities</a>' ?>
+            </li>
+            <li>
+                <?php echo '<a class="btn" href="?controller=Course&action=show">Courses</a>' ?>
+            </li>
+        </ul>
+        <form class="navbar-form navbar-right">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="email">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="password">
+            </div>
+            <button type="submit" class="btn btn-default">Login</button>
+        </form>
     </div>
 </nav>
